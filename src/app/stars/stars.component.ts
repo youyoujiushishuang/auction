48693bc446d417b5964bf50dc238fe0daa4f2514
yj-shuang <497706870@qq.com>
2019-03-15 15:12:43 +0800

    星级组件的星级显示--父组件向子组件传递数据--使用属性绑定

--- src/app/stars/stars.component.ts
+++ src/app/stars/stars.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-stars',
@@ -9,6 +9,11 @@ export class StarsComponent implements OnInit {
 
   //显示五颗星星,需要一个含有五个元素的数组
   private stars:boolean[];
+
+  //需要商品组件中的星级评价数据,要从父组件中接收数据
+  @Input()
+  private rating:number = 0
+
   constructor() { }
 
   ngOnInit() {
