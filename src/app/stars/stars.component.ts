948bd2924132455310355521f7fb6de374e5a353
yj-shuang <497706870@qq.com>
2019-03-15 15:07:26 +0800

    星级评价组件基本样式,空心实心的实现

--- src/app/stars/stars.component.ts
+++ src/app/stars/stars.component.ts
@@ -7,9 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StarsComponent implements OnInit {
 
+  //显示五颗星星,需要一个含有五个元素的数组
+  private stars:boolean[];
   constructor() { }
 
   ngOnInit() {
+    this.stars = [false,false,false,false,false]
   }
 
 }
